3a6e28fcdee3011512db1674eaef4d3777a48b41
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2023-02-22 22:42:43 +0100

    Finished

--- 01 - Drum Kit/main.js
+++ 01 - Drum Kit/main.js
@@ -1,3 +1,19 @@
+const keys = document.querySelectorAll('.keyboard__key');
+
 window.addEventListener('keydown', e => {
-  console.log('js');
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+  if (!audio) return; // stopping function
+  const key = document.querySelector(`.keyboard__key[data-key="${e.keyCode}"]`);
+  
+  audio.currentTime = 0; // audio can be played with no time-between
+  audio.play();
+
+  key.classList.add('playing');
 });
+
+keys.forEach(key => {
+  key.addEventListener('transitionend', e => {
+    if (e.propertyName !== 'transform') return; // skips if it's not a transform
+    e.target.classList.remove('playing');
+  });
+})
