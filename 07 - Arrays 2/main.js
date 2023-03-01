b45fa2b66d8e8547792d78f28739830a7865b5ce
Jakub Kondraciuk <kondraciuk.dev@gmail.com>
2023-03-01 16:07:25 +0100

    Finished

--- 07 - Arrays 2/main.js
+++ 07 - Arrays 2/main.js
@@ -1,1 +1,18 @@
 // people { name, year }, comments { text, id }s
+
+// 1. is at least one person 19?
+const arr1 = people.some((el) => new Date().getFullYear() - el.year >= 19);
+console.log({ arr1 });
+
+// 2. is everyone 19?
+const arr2 = people.every((el) => new Date().getFullYear() - el.year >= 19);
+console.log({ arr2 });
+
+// 3. find comment with ID of 823423
+const arr3 = comments.find(el => el.id === 823423);
+console.log(arr3);
+
+// 4. delete comment with ID of 823423
+const index = comments.findIndex(el => el.id === 823423);
+comments.splice(index, 1);
+console.log(comments);
